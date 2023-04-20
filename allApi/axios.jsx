da8091da1086a229f3407dbb9f67a5835b58a88c
goh-deepak <deepak.gathwal@gohoardings.com>
2023-04-20 16:09:17 +0530

    deepak

--- allApi/axios.jsx
+++ allApi/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const instance =  axios.create({baseURL:"http://localhost:3000/api/"})
+const instance =  axios.create({baseURL:"/api/"})
 instance.defaults.headers.common['Content-Type'] = 'multipart/form-data';
 instance.defaults.withCredentials = true;
 
