c0e2dfddd579613fcd58e4be3e4adb5b148c1e9f
Goh-Deepak <deepak.gathwal@gohoardings.com>
2023-04-26 17:35:02 +0530

    sonjit

---
+++ pages/mydashboard/index.jsx
@@ -0,0 +1,36 @@
+import Fixednavbar from '@/components/navbar/fixednavbar';
+import React from 'react';
+
+const Index = () => {
+  return (
+    <>
+    <Fixednavbar/>
+         <div className=" container-xxl  container-xl container-lg container-md my-5 prf-content">
+        <div className="row  p-5 ">
+        <div className="col-md-3 p-2 pt-0">
+          </div>
+          <div className="col-md-9 p-2 pt-0">
+
+          <h1 className="text-center fw-bold ">My Dashboard </h1>
+          </div>
+          <div className="col-md-3 p-2 pt-0 text-center">
+   
+           
+             
+     
+
+
+
+          </div>
+          <div className="col-md-9 p-2 pt-0">
+            <div className="card ">
+          
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Index;
