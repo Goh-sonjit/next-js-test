20caf9ee781bbb379036c3a71359efd06bf90d5c
goh-uday <uday@gohoardings.com>
2023-05-18 15:35:28 +0530

    asd

--- pages/_document.js
+++ pages/_document.js
@@ -6,25 +6,23 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="../images/web_pics/favicon.png" />
-        <Script strategy="lazyOnload" id="lazyOnload">
-          <script>
-            {`
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-T6SRDVL');
+        <Script strategy="beforeInteractive" id="lazyOnload">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T6SRDVL');
 
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-    
-          gtag('config', 'UA-114762499-1');
-        `}
-          </script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-114762499-1');
+          `}
         </Script>
       </Head>
 
