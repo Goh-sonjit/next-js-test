315c533a77abc17f0f0dd73ce12d5d29d4680e39
goh-deepak <deepak.gathwal@gohoardings.com>
2023-05-20 13:18:15 +0530

    deepak

--- pages/sitemaptransit.xml.js
+++ pages/sitemaptransit.xml.js
@@ -2,25 +2,25 @@ import instance from "@/allApi/axios";
 
 function generateSiteMap(data) {
     const	date = new Date().toISOString()
-  return `<?xml version="1.0" encoding="UTF-8"?>
-  <sitemapindex  xmlns="https://gohoardings.com/">
-     <!--We manually set the two URLs we know already-->
-     
-     ${data
-       .map(({ meta_title, category_name}) => {
-         return `
-       <sitemap>
-       <loc>${`https://gohoardings.com/seedetails/${category_name}/${meta_title}`}</loc>
-             <lastmod>${date}</lastmod>
-             <changefreq>daily</changefreq>
-<priority>0.8</priority>
-       </sitemap>
+    return `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 
-     `;
-       })
-       .join('')}
-   </sitemapindex>
- `;
+    <!--We manually set the two URLs we know already-->
+    
+    ${data
+      .map(({ meta_title, category_name}) => {
+        return `
+      <url>
+          <loc>https://gohoardings.com/seedetails/${category_name}/${meta_title}</loc>
+            <lastmod>${date}</lastmod>
+            <changefreq>daily</changefreq>
+<priority>0.8</priority>
+      </url>
+
+    `;
+      })
+      .join('')}
+  </urlset>
+`;
 }
 
 function SiteMap() {
